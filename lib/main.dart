30972b8bf0634dd6946353b60df731e70b9990b8
costaivxx <gustavodionisio15x@gmail.com>
2026-08-28 15:26:39 -0300

    chore(deps): upgrade firebase_remote_config to ^6.6.0 and tidy analytics imports
    Bump firebase_remote_config to a version compatible with firebase_crashlytics and remove unnecessary analytics import reported by analyzer.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'firebase_options.dart';
 
 import 'services/storage_service.dart';
