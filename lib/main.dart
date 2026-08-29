a638cf1341b645d86073454aaca3434fcda55ec0
costaivxx <gustavodionisio15x@gmail.com>
2026-08-29 10:34:39 -0300

    fix(import): bring in foundation.dart for kReleaseMode used in debug auto-signin guard

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
